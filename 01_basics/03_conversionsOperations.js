2c23bf52727826eb950f1c3720c50b0dbc48722b
Sudhakar Survase <60180044+sudhakarsurvase@users.noreply.github.com>
2024-06-01 11:20:16 +0000

    datatype_summary

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -13,8 +13,36 @@ let score = "Sudhakar"
 
 let isLoggedIn = 0
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1 => true 0 =. false
 //" " => false
 //"Sudhakar" => true
+
+
+// ******************************** Operations****************************************
+
+let value = 3
+let negValue= -value
+
+// console.log(negValue);
+// console.log(2+3);
+// console.log(2-2);
+// console.log(2*5);
+// console.log(2**3);
+// console.log(2/6);
+// console.log(2%5);
+
+let str1 = "hello"
+let str2 = "sudhakar"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log( (3 + 4) * 5 % 6);
+
+// console.log(true);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,41 @@
+// #Primitive
+
+//datatypes 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null 
+let userEmail ;
+
+const id  = Symbol ('123')
+const anotherId = Symbol ('123')
+console.log(id === anotherId);
+
+
+
+// const bigNumber = 2324355735486645n
+
+
+
+
+// Non-Primitive (Reference)
+
+// Array, Object, Functions
+
+ const heros = ["Shaktiman", "Antman" , "Spiderman"];
+
+ let myObj = {
+    name : "Sudhakar",
+    age : 24
+ }
+
+const myFunction = function () {
+    console.log("Hello world");
+ }
+
+ console.log(typeof bigNumber);
