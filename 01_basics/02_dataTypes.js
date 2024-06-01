5c485ebe3deba6893c078ef5bfb999c0c44c2850
Sudhakar Survase <60180044+sudhakarsurvase@users.noreply.github.com>
2024-06-01 10:00:30 +0000

    update datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";   // treat all the js code as a newer version
+
+// alert( 7 + 7 )   // we r using node js, not browser
+
+console.log(7 + 7);
+console.log("Sudhakar")
+let name = "sudhakr";
+let age = 23
+let isLoggedIn = true
+let state;
+
+
+//number => 2 to the power of  53 -1
+//bigint 
+//String = ""
+//boolean => true/false
+//null => standalone value
+//symbol = unique
+
+//Object
+
+console.log(typeof null);    // type of object
+console.log(typeof undefined); // type of undefined
