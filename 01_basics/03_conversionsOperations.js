e78d8782629b503c6e113e692206cbe89c833f7f
Sudhakar Survase <60180044+sudhakarsurvase@users.noreply.github.com>
2024-06-01 10:17:38 +0000

    conversion

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,20 @@
+let score = "Sudhakar"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" =>33
+//"123abc" NaN not a number
+//true 1 or false 0
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1 => true 0 =. false
+//" " => false
+//"Sudhakar" => true
